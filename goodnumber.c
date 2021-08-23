52def50a50e9ceb5e5b8d225cc2b37780bb1ca13
AkashdeepGarg <82059488+AkashdeepGarg@users.noreply.github.com>
2021-08-23 19:37:16 +0530

    Create goodnumber.c
    A number N is called good if the sum of the factors of N, less than N, is equal to the number N. Given a number N, check if it is good or not. Print YES if it's a good number, otherwise print NO.

---
+++ goodnumber.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void) {
+    
+    int n,i=1,sum=0;
+    scanf("%d",&n);
+    
+    while(i<n)
+    {
+        if(n%i==0)
+        {
+            sum=sum+i;
+        }
+        i++;
+    }
+    
+    
+    if(sum==n)
+    {
+       printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+    
+    
+	// your code goes here
+	return 0;
+}
+
